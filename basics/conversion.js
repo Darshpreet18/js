271ef988ab69996f6d85b64184edaa60682aa971
Darshpreet Singh <102509382+Darshpreet18@users.noreply.github.com>
2025-02-11 08:06:01 +0000

    conversion done

---
+++ basics/conversion.js
@@ -0,0 +1,22 @@
+// let score = 'darsh'
+// console.log(typeof score);
+// let value = Number(score)
+// console.log(typeof value);
+// console.log(value);
+// 33 => '33'
+//"33abc"=>NaN
+// true => 1;false =>0
+let bool = 1;
+let checkbool = Boolean(bool);
+// console.log(checkbool);
+// 1=> true; 0=> false
+// "" => false    
+// "darsh"=> true
+let some =33;
+let string = String(some);
+console.log(string);
+console.log(typeof string);  //string
+
+// for checking datavalue as well as type
+
+console.log('2'===2);
