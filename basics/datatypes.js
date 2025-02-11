45abe95728a5f8a5023cd578c15066c8a3112278
Darshpreet Singh <102509382+Darshpreet18@users.noreply.github.com>
2025-02-11 08:06:14 +0000

    datatypes updates

---
+++ basics/datatypes.js
@@ -0,0 +1,8 @@
+const accountId = 120
+let accountEmail = "darsh123@gmail.com"
+accountcity = "jaipur"
+accountEmail ="ha@gmail.com"
+let accountstatement ;
+console.log(accountId);
+console.log(accountstatement);
+console.table([accountId,accountEmail]);
