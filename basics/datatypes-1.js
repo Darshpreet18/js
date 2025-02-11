3339ec9460e8e2b9fe3c890eb642b91b1f45f783
Darshpreet Singh <102509382+Darshpreet18@users.noreply.github.com>
2025-02-11 08:05:44 +0000

    datatypes-1 done

---
+++ basics/datatypes-1.js
@@ -0,0 +1,22 @@
+// Primitive
+// types: String,Number,Boolean,null,undefined,symbol,BigInt
+const score = 100
+const scorevalue  = 100.3
+const isloggedIn = false
+const outtemp = null
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id==anotherid);
+const bignumber = 2222332342342324232323n;
+
+
+//Refernce(Non-Primitive)
+// Array, Objects,Functions
+const arr = [1,2,3,4,5]
+let obj = {name:'John',age:30} 
+const myFunction = function(){
+    console.log('Hello');
+}   
+console.log(typeof bignumber);
+console.log(typeof arr);
+
